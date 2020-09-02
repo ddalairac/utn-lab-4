6f571c1e00085815f4feaa5c0d9d8cedbbdc162f
Diego Dalairac <ddalairac@santandertecnologia.com.ar>
2020-09-02 18:01:35 -0300

    upgrade to angular 10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,69 +1,10 @@
 import { Component } from '@angular/core';
-/* 
-1- (app.component.html) Realizar:
-    una aplicacion que se le ingresen dos edades en dos cuadro de textos
-    edadUno, EdadDos
-    mostrar el promedio y la suma en dos cuadros de textos nuevos
-    botones "calcular" y "limpiar cuadros de textos"
-2-( componentes nuevos) Realizar:
-    la clase usuario con nombre y clave ,
-    el componente "Bienvenido"
-    el componente "login"
-    el componente "error"
-    ( dejar los componentes listos)
-    opcional: hacer ruteo
- */
+
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    public age1: number = 0;
-    public age2: number = 0;
-    public promedio: number = null
-    public suma: number = null
-
-    calcular() {
-        this.suma = this.age1 + this.age2;
-        this.promedio = this.suma / 2;
-        console.log(this.age1,
-            this.age2,
-            this.promedio,
-            this.suma)
-    }
-    limpiar() {
-        this.age1 = 0;
-        this.age2 = 0;
-        this.promedio = null
-        this.suma = null
-    }
-    // routerLinkActive;
-    // navLinks = [
-    //     {
-    //         label: "Bienvenido",
-    //         path: "./Bienvenido",
-    //         isActive: ""
-    //     },
-    //     {
-    //         label: "usuario",
-    //         path: "./usuario",
-    //         isActive: ""
-    //     },
-    //     {
-    //         label: "error",
-    //         path: "./error",
-    //         isActive: ""
-    //     },
-    //     {
-    //         label: "login",
-    //         path: "./login",
-    //         isActive: ""
-    //     },
-    //     {
-    //         label: "contact",
-    //         path: "./contact",
-    //         isActive: ""
-    //     },
-    // ];
+  title = 'lab4';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
-import { UsuarioComponent } from './usuario/usuario.component';
-import { BienvenidoComponent } from './bienvenido/bienvenido.component';
-import { LoginComponent } from './login/login.component';
-import { ErrorComponent } from './error/error.component';
-import { Page404Component } from './page404/page404.component';
-// import { AppRouter } from './app.router';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UsuarioComponent,
-    BienvenidoComponent,
-    LoginComponent,
-    ErrorComponent,
-    Page404Component
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule,
-    FormsModule,,
-    // AppRouter
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]

75a06ee1c4861bd65c035e32c4dc381f10599a84
Diego Dalairac <ddalairac@santandertecnologia.com.ar>
2020-09-03 21:13:05 -0300

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -7,22 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-    constructor() { }
-    usuario:Usuario
+    constructor(public router: Router) { }
+    usuario: Usuario
     ngOnInit(): void {
         this.usuario = new Usuario();
     }
-
+    aceptar() {
+        if (this.usuario.nombre == "admin" && this.usuario.clave == "admin") {
+            this.router.navigateByUrl("bienvenido");
+        } else {
+            this.router.navigateByUrl("error");
+        }
+    }
 
 }
 
-export class Usuario{
-    nombre:string
-    clave:string
-    
-    aceptar() {
-        console.log("log in")
-    }
+export class Usuario {
+    nombre: string
+    clave: string
+
     limpiar() {
         this.nombre = null
         this.clave = null
